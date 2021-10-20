60187a625e172e7309579dc6fad74a3f3aefb871
preethi200027 <chennapreethi.27@gmail.com>
2021-10-20 13:27:21 +0530

    commit

---
+++ python/ajith.js
@@ -0,0 +1,3 @@
+gdcsbchbdsivcbui
+bcsdbicbdsc
+bbjbdjc
---
+++ python/chenna.py
@@ -0,0 +1,11 @@
+hello
+(python.python
+cdsjkhckxnc kbkj csj;)echo "# my-first-repository" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/preethi200027/my-first-repository.git
+git push -u origin main
+git push -- user.name preethi200027
+git push -- user.email "chennapreethi.27@gmail.com"
